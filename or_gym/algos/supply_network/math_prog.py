3bc49d468651516183e1f79dc2e97ccb21a68ad8
hdavid16 <hperez16@gmail.com>
2020-09-19 10:28:15 -0400

    consolidate shrinking horizon and perfect information LP models

--- or_gym/algos/supply_network/math_prog.py
+++ or_gym/algos/supply_network/math_prog.py
@@ -8,7 +8,7 @@ import networkx as nx
 import pandas as pd
 from scipy.stats import *
 
-def im_pi_lp_model(env,use_expectation=False):
+def net_im_pi_lp_model(env,use_expectation=False):
     '''
     Build a perfect information LP model (Oracle) for the InvManagement problem (v2 and v3).
     No policy is used for the reorder. This will give you the optimal reorder quantities if 
@@ -153,169 +153,12 @@ def im_pi_lp_model(env,use_expectation=False):
     
     return lp
 
-def im_sh_lp_model(env,use_expectation=False):
+def net_im_lp_model(env, window_size=np.Inf, use_expectation=False):
     '''
-    Build a shrinking horizon LP model for the InvManagement problem (v2 and v3).
+    Build a rolling/shrinking horizon LP model for the InvManagement problem (v2 and v3).
     The model assumes the average demand from the specified distribution is used
-    (unless a user specified demand profile is given).
-    ''' 
-    
-    #create model
-    lp = pe.ConcreteModel()
-    
-    #define sets
-    lp.J = pe.Set(initialize=env.main_nodes)
-    lp.Jraw = pe.Set(initialize=env.rawmat)
-    lp.Jdistrib = pe.Set(initialize=env.distrib)
-    lp.Jretail = pe.Set(initialize=env.retail)
-    lp.Jmarket = pe.Set(initialize=env.market)
-    lp.Jfactory = pe.Set(initialize=env.factory)
-    lp.network_links = pe.Set(dimen=2, initialize=env.network_links)
-    lp.reorder_links = pe.Set(dimen=2, initialize=env.reorder_links)
-    lp.retail_links = pe.Set(dimen=2, initialize=env.retail_links)
-    lp.demands = pe.Set(dimen=3, initialize=[(t,) + e for e in env.retail_links for t in range(env.num_periods-env.period)])
-    lp.T = pe.RangeSet(0,env.num_periods-1-env.period) 
-    lp.T1 = pe.RangeSet(0,env.num_periods-env.period)
-    
-    #define parameters
-    lp.h = pe.Param(lp.J, initialize = {j:env.graph.nodes[j]['h'] for j in lp.J}) #inventory holding cost at main nodes
-    lp.C = pe.Param(lp.Jfactory, initialize = {j:env.graph.nodes[j]['C'] for j in lp.Jfactory}) #production capacity at each factory node
-    lp.o = pe.Param(lp.Jfactory, initialize = {j:env.graph.nodes[j]['o'] for j in lp.Jfactory}) #operating cost at each factory node
-    lp.v = pe.Param(lp.Jfactory, initialize = {j:env.graph.nodes[j]['v'] for j in lp.Jfactory}) #production yield at each factory node
-    lp.p = pe.Param(lp.network_links, initialize = {e:env.graph.edges[e]['p'] for e in lp.network_links}) #price for selling/purchasing on a link
-    lp.L = pe.Param(lp.reorder_links, initialize = {e:env.graph.edges[e]['L'] for e in lp.reorder_links}) #price for selling/purchasing on a link
-    lp.g = pe.Param(lp.reorder_links, initialize = {e:env.graph.edges[e]['g'] for e in lp.reorder_links}) #price for selling/purchasing on a link
-    lp.b = pe.Param(lp.retail_links, initialize = {e:env.graph.edges[e]['b'] for e in lp.retail_links}) #price for selling/purchasing on a link
-    alpha = env.alpha #time-valued discount 
-    backlog = env.backlog #backlog or lost sales
-    #demand profile
-    D = {e:np.ones(env.num_periods-env.period)*np.mean(env.graph.edges[e]['demand_dist']) if isinstance(env.graph.edges[e]['demand_dist'],(list, np.ndarray)) else np.ones(env.num_periods-env.period)*env.graph.edges[e]['demand_dist'].mean(**env.graph.edges[e]['dist_param']) for e in lp.retail_links} #demands on a retail link for each period
-    lp.D = pe.Param(lp.demands, initialize = {te:D[te[1:]][te[0]] for te in lp.demands}) #store demands
-    prob = {e:[1/(env.num_periods-env.period) for t in range(env.num_periods-env.period)] if isinstance(env.graph.edges[e]['demand_dist'],(list, np.ndarray)) else env.graph.edges[e]['demand_dist'].pmf(D[e],**env.graph.edges[e]['dist_param']) for e in lp.retail_links} #probability of each demand based on distribution
-    lp.prob = pe.Param(lp.demands, initialize = {te:prob[te[1:]][te[0]] for te in lp.demands}) #store probability at each period
-    
-    #define variables
-    lp.X = pe.Var(lp.T1,lp.J,domain=pe.NonNegativeReals) #on hand inventory at each node
-    lp.Y = pe.Var(lp.T1,lp.reorder_links,domain=pe.NonNegativeReals) #pipeline inventory on each link
-    lp.R = pe.Var(lp.T,lp.reorder_links,domain=pe.NonNegativeReals) #reorder quantities for each node
-    lp.S = pe.Var(lp.T,lp.network_links,domain=pe.NonNegativeReals) #sales at each node
-    lp.U = pe.Var(lp.T,lp.retail_links,domain=pe.NonNegativeReals) #unfulfilled sales at each node
-    lp.P = pe.Var(lp.T,lp.J,domain=pe.Reals) #profit at each node
-    
-    #initialize on-hand and pipeline inventories
-    for j in lp.J:
-        lp.X[0,j].fix(env.X.loc[env.period,j])
-        for k in env.graph.predecessors(j):
-            lp.Y[0,k,j].fix(env.Y.loc[env.period,(k,j)])
-    
-    #define constraints
-    lp.profit = pe.ConstraintList()
-    lp.inv_bal = pe.ConstraintList()
-    lp.pip_bal = pe.ConstraintList()
-    lp.reorder1 = pe.ConstraintList()
-    lp.reorder2 = pe.ConstraintList()
-    lp.reorder3 = pe.ConstraintList()
-    lp.sales1 = pe.ConstraintList()
-    lp.sales2 = pe.ConstraintList()
-    lp.sales3 = pe.ConstraintList()
-    lp.unfulfilled = pe.ConstraintList()
-    
-    #build constraints
-    for t in lp.T:
-        for j in lp.J:
-            #profit
-            if j in lp.Jretail:
-                lp.profit.add(lp.P[t,j] == alpha**t * (sum(lp.p[j,k]*lp.S[t,j,k] for k in env.graph.successors(j))
-                                                     - sum(lp.p[k,j]*lp.R[t,k,j] for k in env.graph.predecessors(j))
-                                                     - sum(lp.b[j,k]*lp.U[t,j,k] for k in env.graph.successors(j))
-                                                     - lp.h[j]*lp.X[t+1,j] 
-                                                     - sum(lp.g[k,j]*lp.Y[t+1,k,j] for k in env.graph.predecessors(j))))
-            elif j in lp.Jdistrib:
-                lp.profit.add(lp.P[t,j] == alpha**t * (sum(lp.p[j,k]*lp.S[t,j,k] for k in env.graph.successors(j))
-                                                     - sum(lp.p[k,j]*lp.R[t,k,j] for k in env.graph.predecessors(j))
-                                                     - lp.h[j]*lp.X[t+1,j] 
-                                                     + sum(lp.g[k,j]*lp.Y[t+1,k,j] for k in env.graph.predecessors(j))))
-            elif j in lp.Jfactory:
-                lp.profit.add(lp.P[t,j] == alpha**t * (sum(lp.p[j,k]*lp.S[t,j,k] for k in env.graph.successors(j))
-                                                     - sum(lp.p[k,j]*lp.R[t,k,j] for k in env.graph.predecessors(j))
-                                                     - lp.o[j]/lp.v[j]*sum(lp.S[t,j,k] for k in env.graph.successors(j))
-                                                     - lp.h[j]*lp.X[t+1,j] 
-                                                     + sum(lp.g[k,j]*lp.Y[t+1,k,j] for k in env.graph.predecessors(j))))
-            #on-hand inventory
-            if j in lp.Jdistrib:
-                lp.inv_bal.add(lp.X[t+1,j] == lp.X[t,j] 
-                                            + sum(0 if env.period+t-lp.L[k,j] < 0 else 
-                                                        lp.R[t-lp.L[k,j],k,j] if t-lp.L[k,j] >= 0 else env.R.loc[env.period+t-lp.L[k,j],(k,j)] 
-                                                            for k in env.graph.predecessors(j))
-                                            - sum(lp.S[t,j,k] for k in env.graph.successors(j)))
-            else:
-                lp.inv_bal.add(lp.X[t+1,j] == lp.X[t,j] 
-                                            + sum(0 if env.period+t-lp.L[k,j] < 0 else 
-                                                        lp.R[t-lp.L[k,j],k,j] if t-lp.L[k,j] >= 0 else env.R.loc[env.period+t-lp.L[k,j],(k,j)] 
-                                                            for k in env.graph.predecessors(j))
-                                            - 1/lp.v[j] * sum(lp.S[t,j,k] for k in env.graph.successors(j)))
-            #pipeline inventory
-            for k in env.graph.predecessors(j):
-                if env.period+t-lp.L[k,j] < 0: #if reorder is prior to when the problem started
-                    lp.pip_bal.add(lp.Y[t+1,k,j] == lp.Y[t,k,j] + lp.R[t,k,j])
-                elif t-lp.L[k,j]>=0: #if reorder is in the current horizon scope
-                    lp.pip_bal.add(lp.Y[t+1,k,j] == lp.Y[t,k,j]
-                                                    - lp.R[t-lp.L[k,j],k,j]
-                                                    + lp.R[t,k,j])
-                else: #if reorder is available in history
-                    lp.pip_bal.add(lp.Y[t+1,k,j] == lp.Y[t,k,j]
-                                                    - env.R.loc[env.period+t-lp.L[k,j],(k,j)]
-                                                    + lp.R[t,k,j])
-
-            #reorder quantities
-            if j in lp.Jdistrib and j not in lp.Jretail:
-                lp.reorder1.add(sum(lp.R[t,j,k] for k in env.graph.successors(j)) <= lp.X[t,j])
-            elif j in lp.Jfactory:
-                lp.reorder2.add(sum(lp.R[t,j,k] for k in env.graph.successors(j)) <= lp.X[t,j]*lp.v[j])
-                lp.reorder3.add(sum(lp.R[t,j,k] for k in env.graph.successors(j)) <= lp.C[j])
-
-            #sales quantities
-            if j in lp.Jretail:
-                lp.sales1.add(sum(lp.S[t,j,k] for k in env.graph.successors(j)) <= lp.X[t,j] 
-                                                                                    + sum(0 if env.period+t-lp.L[k,j] < 0 else 
-                                                                                                lp.R[t-lp.L[k,j],k,j] if t-lp.L[k,j] >= 0 else env.R.loc[env.period+t-lp.L[k,j],(k,j)] 
-                                                                                                    for k in env.graph.predecessors(j)))
-                for k in env.graph.successors(j):
-                    if not backlog or env.period+t-1 < 0: #if lost sales or if prevoius period is before the problem started
-                        lp.sales2.add(lp.S[t,j,k] <= lp.D[t,j,k])
-                    elif t-1 >= 0: #if backlog quanity from previous period is known
-                        lp.sales2.add(lp.S[t,j,k] <= lp.D[t,j,k] + lp.U[t-1,j,k])
-                    else: #if backlog quanitty is available in history
-                        lp.sales2.add(lp.S[t,j,k] <= lp.D[t,j,k] + env.U.loc[env.period+t-1,(j,k)])
-            else:
-                for k in env.graph.successors(j):
-                    lp.sales3.add(lp.S[t,j,k] == lp.R[t,j,k])
-
-            #unfulfilled orders
-            if j in lp.Jretail:
-                for k in env.graph.successors(j):
-                    if not backlog or env.period+t-1 < 0:
-                        lp.unfulfilled.add(lp.U[t,j,k] == lp.D[t,j,k] - lp.S[t,j,k])
-                    elif t-1 >= 0:
-                        lp.unfulfilled.add(lp.U[t,j,k] == lp.D[t,j,k] + lp.U[t-1,j,k] - lp.S[t,j,k])
-                    else:
-                        lp.unfulfilled.add(lp.U[t,j,k] == lp.D[t,j,k] + env.U.loc[env.period+t-1,(j,k)] - lp.S[t,j,k])
-
-    #objective function: maximize average profit
-    if use_expectation:
-        lp.obj = pe.Objective(expr = 1/(env.num_periods-env.period) * sum( prod([lp.prob[t,e[0],e[1]] for e in env.retail_links]) * sum(lp.P[t,j] for j in lp.J) for t in lp.T),
-                            sense = pe.maximize)
-    else:
-        lp.obj = pe.Objective(expr = 1/(env.num_periods-env.period) * sum(lp.P[t,j] for j in lp.J for t in lp.T),
-                        sense = pe.maximize)
-    
-    return lp
-
-def im_rh_lp_model(env, window_size, use_expectation=False):
-    '''
-    Build a rolling horizon LP model for the InvManagement problem (v2 and v3).
-    The model assumes the average demand from the specified distribution is used
-    (unless a user specified demand profile is given).
+    For a rolling horizon, specify a window_size; otherwise, a shrinking horizon
+    approach is used.
     ''' 
     
     #adjust window_size towards the end of the simulation (shrinking horizon)
--- or_gym/algos/supply_network/test.py
+++ or_gym/algos/supply_network/test.py
@@ -5,28 +5,28 @@ from pyomo.opt import SolverFactory
 from or_gym.algos.supply_network.math_prog import *
 
 #solve perfect information model
-env1=or_gym.make("InvManagement-v2")
-m1=im_pi_lp_model(env1)
+env1=or_gym.make("InvManagement-v3")
+m1=net_im_pi_lp_model(env1)
 s1=SolverFactory('gurobi')
 res1=s1.solve(m1)
 
 #solve shrinking horizon model at t=0
-env3=or_gym.make("InvManagement-v2")
-m3=im_sh_lp_model(env3)
+env3=or_gym.make("InvManagement-v3")
+m3=net_im_lp_model(env3)
 s3=SolverFactory('gurobi')
 res3=s3.solve(m3)
 
 #solve perfect information model with average demand
-env4=or_gym.make("InvManagement-v2")
+env4=or_gym.make("InvManagement-v3")
 env4.graph.edges[(1,0)]['demand_dist']=[20 for i in range(env4.num_periods)]
-m4=im_pi_lp_model(env4)
+m4=net_im_pi_lp_model(env4)
 s4=SolverFactory('gurobi')
 res4=s4.solve(m4)
 
 #solve shrinking horizon model
-env2=or_gym.make("InvManagement-v2")
+env2=or_gym.make("InvManagement-v3")
 for t in range(env2.num_periods):
-    m2=im_sh_lp_model(env2)
+    m2=net_im_lp_model(env2)
     s2=SolverFactory('gurobi')
     res2=s2.solve(m2)
     Ropt=m2.R.get_values()
@@ -34,24 +34,14 @@ for t in range(env2.num_periods):
     env2.step(action)
 
 #solve rolling horizon model with window = 10
-env5=or_gym.make("InvManagement-v2")
+env5=or_gym.make("InvManagement-v3")
 for t in range(env5.num_periods):
-    m5=im_rh_lp_model(env5,10)
+    m5=net_im_lp_model(env5,window_size=10)
     s5=SolverFactory('gurobi')
     res5=s5.solve(m5)
     Ropt=m5.R.get_values()
     action={e[1:]:Ropt[e] for e in Ropt.keys() if e[0]==0}
     env5.step(action)
-
-#solve rolling horizon model with window = 30
-env6=or_gym.make("InvManagement-v2")
-for t in range(env6.num_periods):
-    m6=im_rh_lp_model(env6,30)
-    s6=SolverFactory('gurobi')
-    res6=s6.solve(m6)
-    Ropt=m6.R.get_values()
-    action={e[1:]:Ropt[e] for e in Ropt.keys() if e[0]==0}
-    env6.step(action)
 
 #show final total profits
 print(np.sum(list(m1.P.get_values().values())))
